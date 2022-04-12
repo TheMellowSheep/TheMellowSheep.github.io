913fe32e5f5e9fec9601aa9da2e2583cd4fcd5e8
Melo <melodiekouy@gmail.com>
2022-04-13 01:02:41 +0200

    Création de dossiers pour améliorer l'organisation

--- board.js
+++ board.js
@@ -6,11 +6,12 @@ const maxColumn = 20;
 
 class Board{
     constructor(height, width){
+        // board's size
         this.line = height / maxLine;
         this.column = width / maxColumn;
         
-        this.board = [];
         // create board
+        this.board = [];
         for( let i = 0; i < this.line; i++){
             this.board.push([]);
             for (let j = 0; j < this.column; j++ ){
@@ -18,5 +19,18 @@ class Board{
             }
         }
     }
+
+    display(){
+
+    }
+
+    coord_to_case(x, y){
+
+    }
+
+    case_to_coord(){
+        // 1 case = 150 x 150 ?
+    }
+    
 }
 
